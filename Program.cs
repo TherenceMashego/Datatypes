3de7298d5a27542b079241e2b878bdfb80e5cafd
TERRENCE  MASHEGO <TERRENCE  MASHEGO@USERDELLFLOOR2>
2023-12-05 16:11:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and initialize variables
+            string name = "John";
+            int age = 35;
+            bool isTall = false;
+            double weight = 80.2;
+
+            // Display information about John
+            Console.WriteLine($"{name} is {age} years old, and he weighs {weight}kg.");
+            Console.WriteLine($"It is {isTall} that he is tall.");
+
+            // Freeze console
+            Console.ReadLine();
+        }
+    }
+}
+
